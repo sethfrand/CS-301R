97a0a26b58be352e5ea36e98f6f1eff68fc743d3
sethfrand <sethp.frandsen@gmail.com>
2026-01-21 20:34:21 -0700

    Added gradio chat that has model dropdown menu to select which model you would like.

---
+++ Homework assignments/homework1c--Chatbot/gradio_chat.py
@@ -0,0 +1,123 @@
+import argparse
+import asyncio
+from pathlib import Path
+
+import gradio as gr
+from openai import AsyncOpenAI, BadRequestError
+
+from usage import *
+
+class ChatAgent:
+    def __init__(self, model:str, prompt:str):
+        self.model = model
+        self.prompt = prompt
+        self.history = []
+        self.usage = []
+        self.ai = AsyncOpenAI()
+        if prompt:
+            self.history.append({'role': 'user', 'content': prompt})
+    async def get_response(self, usr_text: str):
+        self.history.append({'role': 'user', 'content': usr_text})
+        if not usr_text:
+            return ''
+        try:
+            response = await self.ai.responses.create(
+                model=self.model,
+                input=self.history,
+                reasoning={'effort': 'low'}
+            )
+        except BadRequestError as e:
+            if 'reasoning' in str(e).lower():
+                response = await self.ai.responses.create(
+                    model=self.model,
+                    input=self.history
+                )
+            else:
+                raise e
+        
+        self.usage.append(response.usage)
+        self.history.extend(response.output)
+        return response.output_text
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print_usage(self.model, self.usage)
+
+
+async def _main_console(agent):
+    while True:
+        message = input('User: ')
+        if not message:
+            break
+        response = await agent.get_response(message)
+        print('Agent:', response)
+
+
+def _main_gradio(agent):
+    # Constrain width with CSS and center
+    css = """
+    /* limit overall Gradio app width and center it */
+    .gradio-container, .gradio-app, .gradio-root {
+      width: 120ch;
+      max-width: 120ch !important;
+      margin-left: auto !important;
+      margin-right: auto !important;
+      box-sizing: border-box !important;
+    }
+    """
+
+    usage_view = gr.Markdown(format_usage_markdown(agent.model, []))
+
+    with gr.Blocks(css=css, theme=gr.themes.Monochrome()) as demo:
+        with gr.Row():
+            model_dropdown = gr.Dropdown(
+                choices=['gpt-5-nano','gpt-4.1-nano','gpt-5-mini','gpt-5.2-pro'],
+                value = agent.model,
+                label='Model selction')
+
+        def change_model(new_model):
+            agent.model = new_model
+            return format_usage_markdown(agent.model,agent.usage)
+
+        model_dropdown.change(fn=change_model, inputs=model_dropdown, outputs=usage_view)
+        async def get_response(message, chat_view_history):
+            response = await agent.get_response(message)
+            usage_content = format_usage_markdown(agent.model, agent.usage)
+            return response, usage_content
+
+        with gr.Row():
+            with gr.Column(scale=5):
+                bot = gr.Chatbot(
+                    label=' ',
+                    height=600,
+                    resizable=True,
+                )
+                chat = gr.ChatInterface(
+                    chatbot=bot,
+                    fn=get_response,
+                    additional_outputs=[usage_view]
+                )
+
+            with gr.Column(scale=1):
+                usage_view.render()
+
+    demo.launch()
+
+
+def main(prompt_path: Path, model: str, use_web: bool):
+    with ChatAgent(model, prompt_path.read_text() if prompt_path else '') as agent:
+        if use_web:
+            _main_gradio(agent)
+        else:
+            asyncio.run(_main_console(agent))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('ChatBot')
+    parser.add_argument('prompt_file', nargs='?', type=Path, default=None)
+    parser.add_argument('--web', action='store_true')
+    parser.add_argument('--model', default='gpt-5-nano')
+    args = parser.parse_args()
+    main(args.prompt_file, args.model, args.web)
